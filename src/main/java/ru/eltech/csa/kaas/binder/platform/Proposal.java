992f0b56bf9b740c1b5bc1f7f64862b50b755ed6
Raild <Raild@Raild-Note>
2013-04-29 19:42:06 +0400

    Proposal classes hierarсhy has been refactored.

--- src/main/java/ru/eltech/csa/kaas/binder/platform/Proposal.java
+++ src/main/java/ru/eltech/csa/kaas/binder/platform/Proposal.java
@@ -3,7 +3,15 @@ package ru.eltech.csa.kaas.binder.platform;
 import java.util.List;
 import ru.eltech.csa.kaas.binder.model.ServiceImplementation;
 
-public interface Proposal {
+public class Proposal {
 
-    List<ServiceImplementation> getServiceImplementations();
+    private List<ServiceImplementation> serviceImplementations;
+
+    public List<ServiceImplementation> getServiceImplementations() {
+        return serviceImplementations;
+    }
+
+    public void setServiceImplementations(List<ServiceImplementation> serviceImplementations) {
+        this.serviceImplementations = serviceImplementations;
+    }
 }
